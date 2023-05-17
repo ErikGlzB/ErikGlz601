8fd076273852952c2efedfad462b3d1d3aeeb5a3
ErikGlzB <erik.fernando.gb@gmail.com>
2023-05-17 10:45:23 -0600

    Primer version del programa de clase

---
+++ 2_V1_TRIFIV/src/pkg2/v1/trifiv/V1TRIFIV.java
@@ -0,0 +1,36 @@
+package pkg2.v1.trifiv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class V1TRIFIV {
+
+   
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader bufEntrada = new BufferedReader (new InputStreamReader(System.in));
+       
+       //Declaración de variables
+        int numero;
+        float mult3;
+        float mult5;
+        
+        System.out.println("Dame el número; ");
+        numero= Integer.parseInt(bufEntrada.readLine ());
+
+        mult3 = numero%3;
+        mult5 = numero%5;
+        
+        if(mult3 == 0){
+            System.out.println("TRI");
+        }
+        if(mult5 == 0){
+            System.out.println("FIV");
+        }
+        if(mult3==0 && mult5==0){
+            System.out.println("TRIFIV");
+        }
+    }
+    
+}
