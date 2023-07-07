fceb636d47fcc7ae470f19c3f1a70c4dcae4c425
ErikGlzB <erik.fernando.gb@gmail.com>
2023-07-07 10:28:49 -0600

    Se agrega la excepcion 9

---
+++ Excepciones9/src/excepciones9/Excepciones9.java
@@ -0,0 +1,22 @@
+package excepciones9;
+
+public class Excepciones9 {
+
+    public static void main(String[] args) {
+        try {
+            
+            String str = null;
+        
+                int length = str.length(); 
+        
+        System.out.println("La longitud de la cadena es: " + length);
+            
+        } catch (NullPointerException e) {
+            System.out.println("El valor que se mandó a llamar es nulo");
+        }
+    
+
+
+    }
+    
+}
